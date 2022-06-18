923ddc4950a9629ef0317ae1de79695a6a46a831
user <user@user12aa.com>
2022-06-18 03:07:40 +0300

    add gitignore file

--- Library/vars/printMessage.groovy
+++ Library/vars/printMessage.groovy
@@ -1,3 +1,5 @@
 def call(String name, String age ){
+    def jenkinsEnv = new jenkinsenv.EnvInfo(this)
     echo "@@@@@ JobName= ${name}\nBuildNumber= ${age}  @@@@@"
+    echo "########## ${jenkinsEnv.getJobName()}  ---  ${jenkinsEnv.getBuildNumber()}        ###############"
 }
